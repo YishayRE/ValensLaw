89fade5d19ea870139b3ba662006cd90ded1d1ab
YishayJS <yishel.miguel@gmail.com>
2023-03-14 21:20:35 -0600

    First commit

---
+++ js/const.js
@@ -0,0 +1,10 @@
+const url = (window.location.hostname.includes('localhost')) ?
+    'http://localhost:63342/public/' :
+    'https://valenslawfirm.com/'
+
+if (location) {
+    if (/^https:\/\/valenslawfirm.com\/assets\/[^\/]+\/?$/.test(location.href) ||
+        /^http:\/\/valenslawfirm.com\/assets\/[^\/]+\/?$/.test(location.href)) {
+        window.location.href = 'https://valenslawfirm.com/';
+    }
+}
