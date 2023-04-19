7579cc4da649e9f7d05655aadb190987a58aa64e
YishayJS <yishel.miguel@gmail.com>
2023-04-18 20:35:41 -0600

    Cambio de url de prod

--- js/const.js
+++ js/const.js
@@ -1,10 +1,10 @@
 const url = (window.location.hostname.includes('localhost')) ?
     'http://localhost:63342/ValensLaw/' :
-    'https://valens-5694a.web.app/'
+    'https://valenslawfirm.com/'
 
 if (location) {
     if (/^https:\/\/valenslawfirm.com\/assets\/[^\/]+\/?$/.test(location.href) ||
         /^http:\/\/valenslawfirm.com\/assets\/[^\/]+\/?$/.test(location.href)) {
-        window.location.href = 'https://valens-5694a.web.app/';
+        window.location.href = 'https://valenslawfirm.com/';
     }
 }
